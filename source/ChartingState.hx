852cc1a1bb84a1a2067996e3db2df21b73b1b515
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-13 11:57:10 -0300

    Cu de erro

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -43,11 +43,11 @@ import sys.FileSystem;
 
 using StringTools;
 
-#if (flixel >= "5.3.0")
+
 @:access(flixel.sound.FlxSound._sound)
-#else
+
 @:access(flixel.system.FlxSound._sound)
-#end
+
 @:access(openfl.media.Sound.__buffer)
 
 class ChartingState extends MusicBeatState
