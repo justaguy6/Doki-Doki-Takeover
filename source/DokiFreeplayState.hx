a7afa26ec5fe38803d3c66bbc936015cd7820d16
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-15 22:01:23 -0300

    Update DokiFreeplayState.hx

--- source/DokiFreeplayState.hx
+++ source/DokiFreeplayState.hx
@@ -346,13 +346,13 @@ class DokiFreeplayState extends MusicBeatState
 			if (allBeat && !SaveData.popupLibitina)
 				openSubState(new PopupMessage('Libitina', 'freeplay'));
 
-			if (FlxG.keys.justPressed.M || #if android || virtualPad.buttonC.justPressed #end)
+			if (FlxG.keys.justPressed.M #if android || virtualPad.buttonC.justPressed #end)
 			{
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				openSubState(new DokiModifierSubState());
 			}
 
-			if (SaveData.beatProtag && FlxG.keys.justPressed.C || #if android || virtualPad.buttonX.justPressed #end && (curPage != 3 && curPage != 4))
+			if (SaveData.beatProtag && FlxG.keys.justPressed.C #if android || virtualPad.buttonX.justPressed #end && (curPage != 3 && curPage != 4))
 				MusicBeatState.switchState(new CostumeSelectState());
 
 			if (controls.UP_P && !diffselect && (curPage != 3 && curPage != 4))
